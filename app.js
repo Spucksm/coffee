37ce00599c884d4e2df0b9a4ec648d3aca2fdce6
Spenser Spuck <spucksm@gmail.com>
2021-05-27 23:08:52 -0700

    completed day 3 JavaScript

---
+++ app.js
@@ -0,0 +1,17 @@
+//let userName = 'Spenser'
+//userName = 'Tom'
+//console.log(userName)
+
+let userName = prompt('Please Enter Your Name: ');
+console.log(userName);
+document.write('<h2>' + userName+ '</h2>');
+
+let userAnswer = prompt('Do you like Coffee?');
+console.log(userAnswer);
+
+if (userAnswer == 'yes') {
+    document.write('<h3>Welcome ' + userName + '</h3>');
+} else {
+    document.write('<h3>' + userName +' please leave!</h3>');
+    
+}
---
+++ app2.js
@@ -0,0 +1,8 @@
+let areyouagainstcoffee = prompt('Are you against coffee')
+
+if (areyouagainstcoffee == 'no'){
+    document.write('<h3> It is so good</h3>');
+    document.write('<img src="https://image.freepik.com/free-vector/yum-text-with-emoticon_110464-498.jpg" alt="yum face">')
+} else {
+    document.write('<h3> I think you should drink coffee</h3>');
+}
